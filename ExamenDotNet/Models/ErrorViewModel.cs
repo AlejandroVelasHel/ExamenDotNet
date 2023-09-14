87edffc73d30d90ee28a638068b0c02a678115f4
SENA <SENA@MEDAPRCSGFSP540>
2023-09-14 11:53:00 -0500

    Agregar archivos de proyecto.

---
+++ ExamenDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ExamenDotNet/Models/TallerCrudContext.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamenDotNet.Models;
+
+public partial class TallerCrudContext : IdentityDbContext
+{
+    public TallerCrudContext()
+    {
+    }
+
+    public TallerCrudContext(DbContextOptions<TallerCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<DetallesVenta> DetallesVentas { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    public virtual DbSet<Venta> Ventas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=MEDAPRCSGFSP540\\SQLEXPRESS;Initial Catalog=TallerCRUD;integrated security=True; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdClientes).HasName("PK__Clientes__5EB79C21B921DA44");
+
+            entity.HasIndex(e => e.Cedula, "UQ__Clientes__B4ADFE38BB4863C4").IsUnique();
+
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(15)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<DetallesVenta>(entity =>
+        {
+            entity.HasKey(e => e.DetalleVentaId).HasName("PK__Detalles__340EEDA4471E1FA5");
+
+            entity.Property(e => e.Valor).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Producto).WithMany(p => p.DetallesVenta)
+                .HasForeignKey(d => d.ProductoId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__DetallesV__Produ__5441852A");
+
+            entity.HasOne(d => d.Venta).WithMany(p => p.DetallesVenta)
+                .HasForeignKey(d => d.VentaId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__DetallesV__Venta__534D60F1");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProductos).HasName("PK__Producto__718C7D0705982677");
+
+            entity.HasIndex(e => e.Codigo, "UQ__Producto__06370DACD56212D8").IsUnique();
+
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC079349210F");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Fecha).HasColumnType("date");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Venta>(entity =>
+        {
+            entity.HasKey(e => e.IdVenta).HasName("PK__Ventas__BC1240BD829B59CE");
+
+            entity.Property(e => e.FechaVenta)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Total).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Cliente).WithMany(p => p.Venta)
+                .HasForeignKey(d => d.ClienteId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Ventas__ClienteI__5070F446");
+            base.OnModelCreating(modelBuilder);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
